fba0664a33e9b4aacaaa0374fe29b0fc67b94312
imStav <saprocontact@gmail.com>
2023-02-20 15:22:31 +0100

    My Docus Portfolio
    Initializing project!
    
    This repository will contain the source code of my 2023 portfolio. A portfolio made using the Docus theme based on Nuxt 3 and the Nuxt Content module.

---
+++ app.config.ts
@@ -0,0 +1,28 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Steven Averlant | Front-End developer',
+    description: 'The best place to start your documentation.',
+    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    socials: {
+      github: 'imStav',
+    },
+    aside: {
+      level: 0,
+      exclude: []
+    },
+    header: {
+      logo: false,
+      title: '👨‍💻 imStav',
+      showLinkIcon: true,
+      exclude: []
+    },
+    footer: {      
+      credits: 
+        {        
+          icon: '',        
+          text: 'Made w/ ❤️ & ☕ by imStav',        
+          href: 'https://github.com/imStav',      
+        },
+    },
+  }
+})
