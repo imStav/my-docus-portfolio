b835e5f33953ae7d1791e1647aed1a56575f4615
imStav <saprocontact@gmail.com>
2023-02-21 08:13:21 +0100

    config: working on docus config

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   docus: {
     title: 'Steven Averlant | Front-End developer',
-    description: 'The best place to start your documentation.',
+    description: 'Hi there! I am Steven Averlant, a Front-End developer. I mainly work with modern tech stacks such as Vue 3 (Composition API), Nuxt 3 & Nuxt modules, TypeScript.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
       github: 'imStav',
