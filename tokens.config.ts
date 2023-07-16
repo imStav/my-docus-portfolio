649ac6b4e8c047ebaf1fd2076ce67e93d5fc1253
imStav <saprocontact@gmail.com>
2023-07-16 18:46:34 +0200

    feat: add new fonts (WIP)

--- tokens.config.ts
+++ tokens.config.ts
@@ -2,6 +2,14 @@ import { defineTheme, palette } from 'pinceau'
 
 export default defineTheme({
   color: {
-    primary: palette('#1AADFF')
-  }
+    primary: palette('#1AADFF'),
+  },
+  typography: {
+    font: {
+      display: 'ClashDisplay-Variable',
+      body: 'Satoshi-Variable',
+      sans: 'Satoshi-Variable',
+      serif: 'Satoshi-Variable',
+    },
+  },
 })
